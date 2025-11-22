12e5affab02939a52f85247322d69ed738a0c7b3
= <=>
2025-11-23 01:24:27 +0530

    changed to pyrefly

--- init.lua
+++ init.lua
@@ -677,7 +677,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- rust_analyzer = {},
-        pyright = {},
+        pyrefly = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
